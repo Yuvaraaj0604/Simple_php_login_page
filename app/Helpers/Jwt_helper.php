1304b289bb28d7eec9e6b9417a861e7f4645b567
yuvaraaj0602 <yuvaraajkannan@gmail.com>
2026-02-08 20:13:04 +0530

    deploy

--- app/Helpers/Jwt_helper.php
+++ app/Helpers/Jwt_helper.php
@@ -5,7 +5,7 @@ use Firebase\JWT\Key;
 
 function generateJWT(array $user): string
 {
-    $secret = '863a780092f695ee65dbffeeb83c9cf01014e7db4d9af940eb559d998e7c3a10';
+    $secret = env('JWT_SECRET');
 
     if (empty($secret) || strlen($secret) < 32) {
         throw new Exception('JWT_SECRET is missing or too short');
@@ -14,11 +14,11 @@ function generateJWT(array $user): string
     $expiry = env('JWT_EXPIRY') ?? 3600; // default 1 hour
 
     $payload = [
-        'iss'   => 'simple-login',
-        'aud'   => 'simple-login-client',
-        'iat'   => time(),
-        'exp'   => time() + (int)$expiry,
-        'uid'   => $user['id'],
+        'iss' => 'simple-login',
+        'aud' => 'simple-login-client',
+        'iat' => time(),
+        'exp' => time() + (int) $expiry,
+        'uid' => $user['id'],
         'email' => $user['email']
     ];
 
@@ -31,7 +31,7 @@ function validateJWT(string $token)
     try {
         return JWT::decode(
             $token,
-            new Key("863a780092f695ee65dbffeeb83c9cf01014e7db4d9af940eb559d998e7c3a10", 'HS256')
+            new Key(env('JWT_SECRET'), 'HS256')
         );
     } catch (Exception $e) {
         return false;
